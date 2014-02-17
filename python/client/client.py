9514e5666730f859e728a3cb63207d80bf246065
Srinath <g.s.srinath89@gmail.com>
2014-02-17 18:02:25 +0530

    replace all tabs with four spaces

--- python/client/client.py
+++ python/client/client.py
@@ -12,51 +12,51 @@ import random
 
 
 def get_socket(address):
-	sock = socket.create_connection(address)
-	return sock
+    sock = socket.create_connection(address)
+    return sock
 
 def get_start_end():
-	start = random.randrange(1,1000000)
-	end = random.randrange(1,1000000)
-	return (start, end)
+    start = random.randrange(1,1000000)
+    end = random.randrange(1,1000000)
+    return (start, end)
 def collatz_client(host, port, times, pid):
-	try:
-		sock = get_socket((host,port))
-		while times > 0:
-			start, end = get_start_end()
-			query = "%s %s\n"%(start,end)
-			print ("Client id %s sent:"%(pid)),
-			print (query),
-			sock.sendall(query)
-			s = sock.recv(1024)
-			print ("Client id %s received:"%(pid)),
-			print (s),
-			times -= 1
-		sock.close()
-	except socket.error:
-		print "Could not communicate with server. Is the server ready and listening at %s:%s?"%(host, port)
+    try:
+        sock = get_socket((host,port))
+        while times > 0:
+            start, end = get_start_end()
+            query = "%s %s\n"%(start,end)
+            print ("Client id %s sent:"%(pid)),
+            print (query),
+            sock.sendall(query)
+            s = sock.recv(1024)
+            print ("Client id %s received:"%(pid)),
+            print (s),
+            times -= 1
+        sock.close()
+    except socket.error:
+        print "Could not communicate with server. Is the server ready and listening at %s:%s?"%(host, port)
 
 queries = {}
 
 def talk_to_server(host, port, pid):
-	global queries
-	times = random.randrange(1,100)
-	queries[pid] = times
-	collatz_client(host, port, times, pid)
+    global queries
+    times = random.randrange(1,100)
+    queries[pid] = times
+    collatz_client(host, port, times, pid)
 
 
 if __name__ == "__main__":
-	host = "127.0.0.1"
-	port = 8000
-	no_of_clients = int(raw_input("Enter the number of clients from this machine:"))
-	print "Spawning %s clients"%(no_of_clients)
-	start = time.time()
-	coroutines = [gevent.spawn(talk_to_server,host, port, i) for i in xrange(0,no_of_clients)]
-	gevent.joinall(coroutines)
-	end = time.time()
-	tot_queries = 0
-	for k, v in queries.iteritems():
-		tot_queries += v
-	print ""
-	print "============================================================"
-	print "%4d queries processed in %ss time"%(tot_queries,(end - start))
+    host = "127.0.0.1"
+    port = 8000
+    no_of_clients = int(raw_input("Enter the number of clients from this machine:"))
+    print "Spawning %s clients"%(no_of_clients)
+    start = time.time()
+    coroutines = [gevent.spawn(talk_to_server,host, port, i) for i in xrange(0,no_of_clients)]
+    gevent.joinall(coroutines)
+    end = time.time()
+    tot_queries = 0
+    for k, v in queries.iteritems():
+        tot_queries += v
+    print ""
+    print "============================================================"
+    print "%4d queries processed in %ss time"%(tot_queries,(end - start))
--- python/server/server.py
+++ python/server/server.py
@@ -10,32 +10,32 @@ coll = Collatz()
 print "Ready to Serve."
 
 def handler(socket, address):
-	global coll
-	print "Got Connection from %s:%s"%(address)
-	fileobj = socket.makefile()
-	while True:
-		line = fileobj.readline()
-		if not line:
-			print "Client at %s:%s disconnected"%(address)
-			break
-		min_str, max_str = line.strip().split(" ")
-		out = min_str + " " + max_str
-		min_num = int(min_str)
-		max_num = int(max_str)
-		max_cycles = coll.max_cycles_r(min_num, max_num)
-		out += " %s\n"%(max_cycles)
-		fileobj.write(out)
-		fileobj.flush()
-		#print out
-	socket.close()
+    global coll
+    print "Got Connection from %s:%s"%(address)
+    fileobj = socket.makefile()
+    while True:
+        line = fileobj.readline()
+        if not line:
+            print "Client at %s:%s disconnected"%(address)
+            break
+        min_str, max_str = line.strip().split(" ")
+        out = min_str + " " + max_str
+        min_num = int(min_str)
+        max_num = int(max_str)
+        max_cycles = coll.max_cycles_r(min_num, max_num)
+        out += " %s\n"%(max_cycles)
+        fileobj.write(out)
+        fileobj.flush()
+        #print out
+    socket.close()
 
 if __name__ == "__main__":
-	host = "0.0.0.0"
-	port = 8000
-	server = StreamServer((host, port),handler)
-	print "Serving on %s:%s"%(host, port)
-	try:
-		server.serve_forever()
-	except KeyboardInterrupt:
-		print "Quitting..."
-		exit(0)
+    host = "0.0.0.0"
+    port = 8000
+    server = StreamServer((host, port),handler)
+    print "Serving on %s:%s"%(host, port)
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print "Quitting..."
+        exit(0)
